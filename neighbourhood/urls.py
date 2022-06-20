cef8a7b0c3a168fa4f66ebe0ea33a503a1ed7ca2
annk1995 <kabuikariuki052@gmail.com>
2022-06-21 02:08:40 +0300

    made changes to runtime

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -7,8 +7,7 @@ Function views
     
 
    
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+   
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
