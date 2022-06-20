f3ce65fe0eea46671c8e0edbdf399b5d3d2311b3
annk1995 <kabuikariuki052@gmail.com>
2022-06-21 01:51:18 +0300

    admin superuser migrations

--- watch/admin.py
+++ watch/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
+
 admin.site.register(NeighbourHood)
 admin.site.register(Profile)
 admin.site.register(Business)
