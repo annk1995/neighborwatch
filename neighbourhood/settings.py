9654fdd6293b43832fbd0df471277ce464d20a85
annk1995 <kabuikariuki052@gmail.com>
2022-06-21 02:05:29 +0300

    migrations

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -15,7 +15,7 @@ import cloudinary.api
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 LOGIN_REDIRECT_URL='/'
