20ac25ca5a92baecd57e3db8196489c25e093043
annk1995 <kabuikariuki052@gmail.com>
2022-06-21 02:06:04 +0300

    made changes to urls

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -6,7 +6,7 @@ Examples:
 Function views
     
 
-    1. Add an import:  from other_app.views import Home
+   
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
